5196d07c3f57d8996d0682ab63248af75713d37a
pedromapelli <pedro.m@lyncas.net>
2022-04-07 18:58:29 -0300

    feat: Criando Produto e Categoria

---
+++ src/NerdStore.Catalogo.Domain/Categoria.cs
@@ -0,0 +1,21 @@
+﻿using NerdStore.Core.DomainObjects;
+
+namespace NerdStore.Catalogo.Domain
+{
+    public class Categoria : Entity
+    {
+        public Categoria(string nome, int codigo)
+        {
+            Nome = nome;
+            Codigo = codigo;
+        }
+
+        public string Nome { get; private set; }
+        public int Codigo { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{Nome} - {Codigo}";
+        }
+    }
+}
