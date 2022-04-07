db7aa8b1d42b1c2addeec6a398d106629dd43964
pedromapelli <pedro.m@lyncas.net>
2022-04-07 20:26:22 -0300

    feat: Implementando serviços de domínio

---
+++ src/NerdStore.Catalogo.Domain/EstoqueService.cs
@@ -0,0 +1,39 @@
+﻿namespace NerdStore.Catalogo.Domain
+{
+    public class EstoqueService : IEstoqueService
+    {
+        private readonly IProdutoRepository _produtoRepository;
+
+        public EstoqueService(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
+        public async Task<bool> DebitarEstoque(Guid produtoId, int quantidade)
+        {
+            Produto produto = await _produtoRepository.ObterPorId(produtoId);
+
+            if (!produto.PossuiEstoque(quantidade)) return false;
+
+            produto.DebitarEstoque(quantidade);
+
+            _produtoRepository.Atualizar(produto);
+            return await _produtoRepository.UnitOfWork.Commit();
+        }
+
+        public async Task<bool> ReporEstoque(Guid produtoId, int quantidade)
+        {
+            Produto produto = await _produtoRepository.ObterPorId(produtoId);
+
+            produto.ReporEstoque(quantidade);
+
+            _produtoRepository.Atualizar(produto);
+            return await _produtoRepository.UnitOfWork.Commit();
+        }
+
+        public void Dispose()
+        {
+            _produtoRepository.Dispose();
+        }
+    }
+}
